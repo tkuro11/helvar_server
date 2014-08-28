870d0a78df0a5d58b3fce5bce0af11c8bce0505c
Takayuki Kurosawa <takayuki.kurosawa.yk@renesas.com>
2014-08-28 13:50:16 +0900

    XML parser job rewritten.

--- helvar_server/routes/index.js
+++ helvar_server/routes/index.js
@@ -4,39 +4,54 @@ var directlevel = require("./directlevel");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.sendFile('main_console.html', {root:"./public/"});
-	console.log("/writed");
+    res.sendFile('main_console.html', {root:"./public/"});
+    console.log("/writed");
 });
 
 router.post('/', function(req, res) {
-	var ipaddr = req.client.remoteAddress;
-	var MACaddr	= req.body.lvdata['$']['MACaddr'];
-	var ID = Number(req.body.lvdata.lightid);
-	console.log(MACaddr);
+    var ipaddr = req.client.remoteAddress;
+    var MACaddr	= req.body.lvdata['$']['MACaddr'];
+    var ID = Number(req.body.lvdata.lightid);
+    console.log(MACaddr+ ":"+ ID);
+    settings.IPDICT[ipaddr] = MACaddr;
 
-	if (isNaN(ID)) {
-		res.send("NOID");
-		return ;
-	}
+    if (isNaN(ID)) {
+	res.send("NOID");
+	return ;
+    }
 
-	ID = ((ID-1)%8)+1;
+    ID = ((ID-1)%8)+1;
 
-	// register IP - MAC assoc
-	settings.IPDICT[ipaddr] = MACaddr;
+    // register IP - MAC assoc
+    // update?
+    var termno = settings.MACDICT[MACaddr];
 
-	// update?
-	var termno = settings.MACDICT[MACaddr];
+    if (settings.selected[termno] != ID) {
+	// update!
+	var prev = settings.selected[termno];
+	directlevel.rgb(prev, settings.default_color, 100);
+	directlevel.rgb(ID, settings.colors[termno], 100);
+	settings.selected[termno] = ID;
+    }
 
-	if (settings.selected[termno] != ID) {
-		// update!
-		var prev = settings.selected[termno];
-		directlevel.rgb(prev, settings.default_color);
-		directlevel.rgb(ID, settings.colors[ID]);
-		settings.selected[termno] = ID;
-	}
-
-	res.send("OK");
+    res.send("OK");
 });
 
+router.get('/colors', function(req, res) {
+    res.send(settings.ecoselector_colors);
+    console.log("colors");
+});
+
+router.post('/updatecolor', function(req, res) {
+    console.log("/updater");
+    console.log(req.body.color);
+    var MACaddr = settings.IPDICT[req.client.remoteAddress];
+    var termno = settings.MACDICT[MACaddr];
+    settings.colors[termno] = req.body.color;
+    directlevel.rgb(settings.selected[termno], req.body.color);
+    console.log("--" + settings.colors + "," + req.client.remoteAddress);
+    console.log(settings.IPDICT);
+    res.send("OK");
+});
 
 module.exports = router;
